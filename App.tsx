b96c1b39806fcc901b9df6c359c1e421de1f0238
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-04-21 01:52:15 -0300

    Primeira página do App

--- App.tsx
+++ App.tsx
@@ -6,28 +6,3 @@ export default function App() {
     <Welcome />
   )
 }
-
-// PRIMEIRO EXEMPLO...
-
-// import { StatusBar } from 'expo-status-bar';
-// import React from 'react';
-// import { StyleSheet, Text, View } from 'react-native';
-
-// export default function App() {
-//   return (
-//     <View style={styles.container}>
-//       <Text>Hello World!</Text>
-//       <Text>Welcome to NLW#05</Text>
-//       <StatusBar style="auto" />
-//     </View>
-//   );
-// }
-
-// const styles = StyleSheet.create({
-//   container: {
-//     flex: 1,
-//     backgroundColor: '#FFBD00',
-//     alignItems: 'center',
-//     justifyContent: 'center',
-//   },
-// });
