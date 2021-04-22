55451fb436ce1a4388d91695b303922c89efcbe4
Luciano Barreto <contato.lucianobarreto@gmail.com>
2021-04-22 01:44:24 -0300

    Segunda página e identificação do usuário

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,40 +1,32 @@
 import React from 'react';
-import  { TouchableOpacity, StyleSheet, Text, TouchableOpacityProps } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+
 import colors from '../styles/colors';
+import fonts from '../styles/fonts';
 
-interface ButtonProps extends TouchableOpacityProps {
-  title: string;
-}
-
-export function Button({ title, ...rest } : ButtonProps ) {
+export function Button() {
   return (
-    <TouchableOpacity 
-      style={styles.button} 
-      activeOpacity={0.7}
-      {...rest}
-    >
-    <Text style={styles.buttonText}>
-      { title }
-    </Text>
+    <TouchableOpacity style={styles.container}>
+      <Text style={styles.text}>
+        Confirmar
+      </Text>
     </TouchableOpacity>
   )
-}
+};
 
 const styles = StyleSheet.create({
-  button: {
+  container: {
     backgroundColor: colors.green,
-    justifyContent: 'center',
-    alignItems: 'center',
-    borderRadius: 16,
-    marginBottom: 10,
     height: 56,
-    width: 56,
-    paddingHorizontal: 10
+    borderRadius: 16,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
 
-  buttonText: {
-    color:colors.white,
-    fontSize: 24
+  text: {
+    fontSize: 16,
+    color: colors.white,
+    fontFamily: fonts.heading
   }
 
-});
+})
